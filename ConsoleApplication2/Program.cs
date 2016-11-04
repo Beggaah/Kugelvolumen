c9673a13ed2e11e08fc9768b955656af1ad98a6d
Johannes Becker <j.becker@faz.de>
2016-11-04 15:56:50 +0100

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ErstesCSharpPrgrV3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Berechnung eines Kugelvolumens
+            double r, v;     
+            Console.WriteLine("Berechnung eines Kugelvolumens:\n");
+            Console.Write("Radius r= ");
+            r = Convert.ToDouble(Console.ReadLine());
+            v = ((double) 4/3) * Math.PI * (r * r * r);  // Integerdivision !!! -> Parse to double!!!
+            Console.WriteLine("Das Kugelvolumen  v= (4/3)*{0}*({1}^3)= {2}", Math.PI, r, v);
+            Console.ReadKey();
+        }
+    }
+}
